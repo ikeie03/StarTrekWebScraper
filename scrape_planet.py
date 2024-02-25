78865d695ac16992fdb2e629c8b04812052e0da2
Liza George <liza@chiramattel.com>
2024-02-24 18:02:12 -0600

    Add reminder

--- scrape_planet.py
+++ scrape_planet.py
@@ -5,25 +5,26 @@ from bs4 import BeautifulSoup
 planets = []
 with open('planets.txt', 'r') as file:
     planets = file.readlines()
+    # TODO: replace all spaced and special chars w underscores etc
 
 for planet in planets:
     url = "https://memory-alpha.fandom.com/wiki/" + planet
     print(url)
 
     planet_page = urlopen(url)
-    page_html = planet_page.read()
-    planet_page.close()
-    html_soup = BeautifulSoup(page_html, 'html.parser')
+    # page_html = planet_page.read()
+    # planet_page.close()
+    # html_soup = BeautifulSoup(page_html, 'html.parser')
 
-    # The status always follows this html element:
-    # <h3 class="pi-data-label pi-secondary-font">Status:</h3>
-    status_label = html_soup.find('h3', class_='pi-data-label pi-secondary-font')
+    # # The status always follows this html element:
+    # # <h3 class="pi-data-label pi-secondary-font">Status:</h3>
+    # status_label = html_soup.find('h3', class_='pi-data-label pi-secondary-font')
 
-    status_div = status_label.find_next_sibling('div')
+    # status_div = status_label.find_next_sibling('div')
 
-    status = status_div.text.strip()
+    # status = status_div.text.strip()
 
-    print(status)
+    # print(status)
 
 
 

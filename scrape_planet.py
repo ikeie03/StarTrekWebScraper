2ffb879e7b698b30a96a124ca53dca925668506e
Liza George <liza@chiramattel.com>
2024-02-24 18:39:18 -0600

    Clean planet names and push output to csv

--- scrape_planet.py
+++ scrape_planet.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 
 import ssl
 import certifi
-from urllib.request import urlopen
+
+import csv
 
 
 planets = []
@@ -12,7 +13,20 @@ with open('planets.txt', 'r') as file:
     planets = file.readlines()
     # TODO: replace all spaced and special chars w underscores etc
 
-for planet in planets:
+scraped_results = [["planet_name", "status"]]
+with open("scraped_results.csv", 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(scraped_results)
+scraped_results = []
+
+for (idx, planet) in enumerate(planets):
+    # ignore any planet categories
+    if "Category:" in planet:
+        continue
+
+    # clean the planet string of spaces and newlines
+    planet = planet.replace(" ", "_").rstrip('\n')
+
     url = "https://memory-alpha.fandom.com/wiki/" + planet
 
     ssl_context = ssl.create_default_context(cafile=certifi.where())
@@ -25,10 +39,21 @@ for planet in planets:
     # The status always follows this html element:
     # <h3 class="pi-data-label pi-secondary-font">Status:</h3>
     status_label = html_soup.find('h3', class_='pi-data-label pi-secondary-font', string='Status:')
+
+    # Ignore planets without status
+    if status_label == None:
+        continue
+
     status_div = status_label.find_next_sibling('div')
     status = status_div.text.strip().split()[0]
 
-    print(status)
+    # print(status)
+    scraped_results.append([planet, status])
 
+    # write results to file every 100 planets
+    if idx % 100 == 0:
+        with open("scraped_results.csv", 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(scraped_results)
 
-
+        scraped_results = []
